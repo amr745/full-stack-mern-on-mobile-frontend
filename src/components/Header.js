1b2aaa9c63d1ccbb9027a05a263218e4e50d7c78
Ashley Richard <ashleymrichard@gmail.com>
2022-06-08 04:32:53 -0700

    Header Has Home Link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
-// import { Link } from "react-router-dom";
-import React from 'react';
+import { Link } from "react-router-dom";
+import React from "react"
+import Home from "../pages/Home";
 
 function Header(props) {
 //   inline style for the nav tag
@@ -11,10 +12,40 @@ function Header(props) {
   //   width: "90%",
   //   margin: "auto",
   // };
+
+  // const [image, setImage] = useState(null);
+
+  // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
+  // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
+
+  // const handleClick = (Home) => {
+    // setImage(Home)
+  // };
+
+  // CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
+  // ASSIGN ALL OF THE PROPERTIES THAT IT NEEDS: src, alt, className, key INCLUDING AN onClick EVENT THAT CALLS THE HANDLE EVENT FUNCTION AND PASSES IT THE IMG URL
   
+  // const images = imagesArr.map((ele, index) => {
+  //   return (
+  //   <img className="thumb" src={ele.img} alt={ele.alt} key={index} onClick={()=>handleClick(ele.img)} />
+  //   )
+  // });
+
+  
+
+  // make an initial call for the data inside a useEffect, so it only happens once on component load
+  // useEffect(() => {getImage()}, []);
+
+  // const handleClick = () => {
+    // setImage(props.URL)
+  // };
+
   return (
+    // return home.map((home) => (
   <header>
-      <img src="https://imgur.com/84nWw4D.png" alt="Shop Logo" />
+    <Link to={`/`}>
+      <img src="https://imgur.com/84nWw4D.png" alt="Shop Logo"/>
+    </Link>
       {/* <nav style={navStyle}>
           <Link to="/">HOME</Link>
           <Link to="/Services">SERVICES</Link>
